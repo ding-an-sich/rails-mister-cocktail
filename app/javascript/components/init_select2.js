813cb4936a473e7f261f743c89ae74f913b4bd3f
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-08-27 16:52:59 -0300

    Tacky navbar

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('#dose_ingredient_id').select2();
-  $('#dose_ingredient_id').select2().on('select2:open', (event) => {
+  $('#dose_ingredient_id').select2(); // Add select2 to element
+  $('#dose_ingredient_id').select2().on('select2:open', (event) => {  // Add placeholder to Select2
     $('.select2-search__field').attr('placeholder', 'Type to search...');
   })
 };
