c4d0f3eafbf624a35689a249719045d265a9e634
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-08-27 16:24:14 -0300

    Added placeholder value to Select2 search field

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -3,6 +3,9 @@ import 'select2';
 
 const initSelect2 = () => {
   $('#dose_ingredient_id').select2();
+  $('#dose_ingredient_id').select2().on('select2:open', (event) => {
+    $('.select2-search__field').attr('placeholder', 'Type to search...');
+  })
 };
 
 export { initSelect2 };
