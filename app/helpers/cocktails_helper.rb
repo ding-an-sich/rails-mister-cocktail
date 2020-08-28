37376e6b00d9de5319dd9f676153ab8318533e07
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-08-28 13:51:24 -0300

    More helpers for cocktail photos

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,9 +1,16 @@
 # Default helper for the CocktailsController
 module CocktailsHelper
-  # Check if there is a photo attached and display the default one if not
-  def photo_check(cocktail)
+  # Check if there is a photo attached and return the path of default one if not
+  def photo_check_path(cocktail)
     return cl_image_path(cocktail.photo.key) if cocktail.photo.attached?
 
     image_path('rabo-de-galo.jpg') # HTML helpers are included in this helper module by default
+  end
+
+  # Same as above, but uses tag helper
+  def photo_check_tag(cocktail)
+    return cl_image_tag(cocktail.photo.key, class: 'img-cock') if cocktail.photo.attached?
+
+    image_tag('rabo-de-galo.jpg', class: 'img-cock')
   end
 end
